74b4624a90aeda0196f912447ea825522f60e189
natnael <natnaelmessay477@gmail.com>
2024-10-07 11:06:45 +0300

    add vercer.jason30

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ const TelegramBot = require('node-telegram-bot-api');
 
 // Replace with your own Bot Token from BotFather
 const token = '7865623009:AAHaE6moxB6jQPDzeTpjS-yTqxltWGnov10';
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token);
+
+// The handler function
+module.exports.telegramWebhook = async (event) => {
+    const update = JSON.parse(event.body);
+    console.log(update);cls
+    bot.processUpdate(update);
+
+    return {
+        statusCode: 200,
+        body: 'Webhook received',
+    };
+};
 
 // Start command handler
 bot.onText(/\/start/, (msg) => {
@@ -11,12 +23,10 @@ bot.onText(/\/start/, (msg) => {
     const options = {
         reply_markup: {
             inline_keyboard: [
-                [{ text: 'Open User Info', url: 'https://nat-coin-oxyg.vercel.app/' }]
+                [{ text: 'Open User Info', url: '' }]
             ]
         }
     };
 
     bot.sendMessage(chatId, 'Welcome! Click the button below to open the user info app.', options);
 });
-
-console.log('Bot is running...');
